bb2214fd0316ab1cb8b903d6cc6f6977bf2b0c55
Simon Lecoq <simon.lecoq@corp.ovh.com>
2022-09-22 23:03:03 +0200

    feat(tests): improve mocked server

--- tests/server.go
+++ tests/server.go
@@ -1,17 +1,31 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 )
 
 func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+
 	fs := http.FileServer(http.Dir("./tests/static"))
 	http.Handle("/", fs)
 
-	log.Print("Listening on :3000...")
-	err := http.ListenAndServe(":3000", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	http.HandleFunc("/ready", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Ready"))
+	})
+
+	http.HandleFunc("/stop", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Stopping mock server"))
+		cancel()
+	})
+
+	go func() {
+		if err := http.ListenAndServe(":3000", nil); err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
+
+	<-ctx.Done()
 }
